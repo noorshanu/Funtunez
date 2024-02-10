4aeef897a65f8297088158b32510baeacddd2970
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-02-11 03:28:48 +0530

    Update MarqueSection.jsx

--- src/components/MarqueSection.jsx
+++ src/components/MarqueSection.jsx
@@ -5,7 +5,9 @@ function MarqueSection() {
   return (
     <>
       <Marquee>
+      <p className=" text-grad p-4">!!!!!!</p>
       <img src=" images/logo.png" alt="" className=" h-[50px] p-4" />
+      <p className=" text-grad p-4">!!!!!!</p>
 
         <p className="font-64 text-grad p-4">Exclusive NFTs</p>
 
@@ -15,11 +17,21 @@ function MarqueSection() {
         <p className=" text-grad p-4">!!!!!!</p>
 
         <img src="images/fav.png" alt="" className=" h-[60px] p-4" />
+        <p className=" text-grad p-4">!!!!!!</p>
 
-        <p className="font-64 text-grad">Exclusive NFTs</p>
+        <p className="font-64 text-grad">Fantunez</p>
         <p className=" text-grad p-4">!!!!!!</p>
         <img src=" images/logo.png" alt="" className=" h-[50px] p-4" />
-        <p className="font-64 text-grad p-4">Metaverse Experiences</p>
+        <p className=" text-grad p-4">!!!!!!</p>
+        <p className="font-64 text-grad p-4">VR</p>
+        <p className=" text-grad p-4">!!!!!!</p>
+        
+        <p className="font-64 text-grad p-4">AR</p>
+<p className=" text-grad p-4">!!!!!!</p>
+<p className="font-64 text-grad p-4 capitalize">Exclusive fan tokens</p>
+<p className=" text-grad p-4">!!!!!!</p>
+
+        
       </Marquee>
     </>
   );
