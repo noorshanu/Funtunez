52fb726fe51b17f82f70eaac2b73995a00adb04c
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-02-11 03:33:08 +0530

    new

--- src/sections/Collab.jsx
+++ src/sections/Collab.jsx
@@ -9,26 +9,17 @@ function Collab() {
         <div className=' bg-primary shadow-md p-4 rounded-lg '>
 
         <div className=" flex justify-between flex-col sm:flex-row gap-4">
-          <div className="nft border-[#112411] border p-4 relative bg-grad">
-            <img
-              src="images/about.jpg"
-              alt=""
-              className=" rounded-2xl relative z-[100] h-[400px] "
-            />
-          </div>
+      
           <div className=" max-w-xl ">
             <div className=" my-3">
               <h1 className="text-grad text-4xl font-work font-bold font-64">
-                About US{" "}
+              For Creators
               </h1>
-              <div className="bg-white h-[3px] w-[300px] my-2"> </div>
+              <div className="bg-white h-[3px] w-[430px] my-2"> </div>
             </div>
             <p className=" text-white text-lg">
-              Welcome to FanTunez, a dynamic ecosystem designed to power the
-              creator economy. We're on a mission to provide a unique platform
-              where creators can monetize their art and connect with fans
-              through immersive metaverse experiences, exclusive NFTs, events,
-              and merchandise.
+            we mention about what we do for creators. Here we will mention about #create2earn as well
+
             </p>
             <p className="text-white text-lg py-4">
               We believe in the trans-formative power of the creator economy,
@@ -53,6 +44,14 @@ function Collab() {
               </div>
             </div>
             <div></div>
+          </div>
+
+          <div className="nft border-[#112411] border p-4 relative bg-grad">
+            <img
+              src="images/creat.jpg"
+              alt=""
+              className=" rounded-2xl relative z-[100] h-[400px] "
+            />
           </div>
         </div>
 
