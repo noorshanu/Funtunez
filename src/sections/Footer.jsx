e1a689a3f84661fce4b06928b2f2a19dc56fd474
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-02-12 04:09:44 +0530

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,4 +1,5 @@
-import { FaTelegram } from "react-icons/fa";
+import { FaTelegram,FaInstagram,FaFacebook, } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 function Footer() {
   return (
@@ -7,46 +8,47 @@ function Footer() {
         <div className=" grid grid-cols-2 sm:grid-cols-4 justify-evenly items-center sm:items-start gap-8 pb-8">
           <div>
             <img src="images/foot.png" alt="" className=" h-auto sm:h-[200px] mx-auto" />
-            <div className=" flex justify-center  items-center gap-4">
+      
+          </div>
+          <div>
+            <h1 className=" text-white text-2xl font-work font-medium ">Important Links</h1>
+
+            <p className="text-[#fff] hover:text-[#8ae058] text-lg">How it Works</p>
+            <p className="text-[#fff] hover:text-[#8ae058] text-lg">How it Works</p>
+            <p className="text-[#fff] hover:text-[#8ae058] text-lg">How it Works</p>
+            <p className="text-[#fff] hover:text-[#8ae058] text-lg">How it Works</p>
+            <p className="text-[#fff] hover:text-[#8ae058] text-lg">How it Works</p>
+          </div>
+
+          <div>
+            <h1 className="text-white text-2xl font-work font-medium ">Features</h1>
+
+            <p className="text-[#fff] hover:text-[#8ae058] text-lg">How it Works</p>
+            <p className="text-[#fff] hover:text-[#8ae058] text-lg">How it Works</p>
+            <p className="text-[#fff] hover:text-[#8ae058] text-lg">How it Works</p>
+            <p className="text-[#fff] hover:text-[#8ae058] text-lg">How it Works</p>
+            <p className="text-[#fff] hover:text-[#8ae058] text-lg">How it Works</p>
+          </div>
+
+          <div>
+            <div className=" flex gap-2 flex-col sm:flex-row text-center justify-center mx-auto">
+              <input type="text" placeholder="enter your mail" className=" px-4 w-[160px] sm:w-auto mb-4 sm:mb-0 border-[#47ef47] text-white bg-transparent border-2 rounded-full  " />
+              <button className="py-2 px-10    font-medium button-62 text-lg rounded-full border border-[#4d4c4c]">submit</button>
+            </div>
+            <div className=" flex mt-4    gap-4">
               <a href="/" className=" text-[#fff] hover:text-[#8ae058] text-3xl">
                 <FaTelegram />
               </a>
               <a href="/" className=" text-[#fff] hover:text-[#8ae058] text-3xl">
-                <FaTelegram />
+              <FaInstagram />
               </a>
 
               <a href="/" className="text-[#fff] hover:text-[#8ae058] text-3xl">
-                <FaTelegram />
+              <FaFacebook />
               </a>
               <a href="/" className=" text-[#fff] hover:text-[#8ae058] text-3xl">
-                <FaTelegram />
+              <FaSquareXTwitter />
               </a>
-            </div>
-          </div>
-          <div>
-            <h1 className=" text-white text-2xl font-work font-medium ">About</h1>
-
-            <p>How it Works</p>
-            <p>How it Works</p>
-            <p>How it Works</p>
-            <p>How it Works</p>
-            <p>How it Works</p>
-          </div>
-
-          <div>
-            <h1 className="text-white text-2xl font-work font-medium ">About</h1>
-
-            <p>How it Works</p>
-            <p>How it Works</p>
-            <p>How it Works</p>
-            <p>How it Works</p>
-            <p>How it Works</p>
-          </div>
-
-          <div>
-            <div className=" flex sm:block flex-col text-center justify-center mx-auto">
-              <input type="text" className=" px-4 w-[160px] sm:w-auto mb-4 sm:mb-0" />
-              <button className="py-2 px-10    font-medium button-62 text-lg rounded-full border border-[#4d4c4c]">submit</button>
             </div>
           </div>
         </div>
